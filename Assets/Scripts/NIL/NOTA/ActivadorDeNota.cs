91892c8760d42eb4dfd89d67e065d57c327b9064
nilibanez <97691830+nilibanez@users.noreply.github.com>
2022-04-04 17:31:28 +0200

    nota

---
+++ Assets/Scripts/NIL/NOTA/ActivadorDeNota.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivadorDeNota : MonoBehaviour
+{
+    public GameObject notaVisual;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
