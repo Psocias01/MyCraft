f914e733d81003927b60b18a0eef332a1d3f2725
nilibanez <97691830+nilibanez@users.noreply.github.com>
2022-04-05 17:19:01 +0200

    nota

--- Assets/Scripts/NIL/NOTA/ActivadorDeNota.cs
+++ Assets/Scripts/NIL/NOTA/ActivadorDeNota.cs
@@ -1,14 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActivadorDeNota : MonoBehaviour
 {
     public GameObject notaVisual;
+    public bool activa;
+    
+    public GameObject textLetra;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && activa == true)
+        {
+            notaVisual.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && activa == true)
+        {
+            notaVisual.SetActive(false);
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            activa = true;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            activa = false;
+            notaVisual.SetActive(false);
+        }
+    }
+    
+    public void OpenMessagePanel(string text)
+    {
+        textLetra.SetActive(true);
+    }
+
+    public void CloseMessagePanel()
+    {
+        textLetra.SetActive(false);
     }
 }
